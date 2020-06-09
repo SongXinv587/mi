8088aa8ca3eacd80d795088147fcfc0606d822e9
ss <1272378043@qq.com>
2020-06-09 14:15:19 +0800

    vue写

--- mi/mi_store/src/App.vue
+++ mi/mi_store/src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
     <router-view />
+    <!-- <footerbar></footerbar> -->
   </div>
 </template>
 
 <style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
 </style>
+<script>
+// import footerbar from "./views/footerbar";
+// export default {
+//   components: {
+//     footerbar
+//   }
+// };
+</script>
---
+++ mi/mi_store/src/views/footerbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <footer>
+    <ul>
+      <li>
+        <a href>
+         <div></div>
+        </a>
+      </li>
+      <li>
+        <a href>
+          <img src alt />
+        </a>
+      </li>
+      <li>
+        <a href>
+          <img src alt />
+        </a>
+      </li>
+      <li>
+        <a href>
+          <img src alt />
+        </a>
+      </li>
+      <li>
+        <a href>
+          <img src alt />
+        </a>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<style lang="less">
+footer {
+  height: 52px;
+  background: #fff;
+  border: 0.5px solid #999;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  box-sizing: border-box;
+  //   padding: 10px 0;
+  box-sizing: border-box;
+  z-index: 999;
+  ul {
+    display: flex;
+    margin: 15px 0;
+    justify-content: space-around;
+    text-align: center;
+    li {
+      flex: 1;
+    //   &:nth-child(1){
+    //       background-image: url(@/assets/imgs/houselogo.png) no-repeat;
+    //   }
+    }
+  }
+}
+</style>
+<script>
+// import navbox from "./components/navbox.vue"
+// export default {
+//     components:{
+//         // navbox
+//     }
+// }
+</script>
